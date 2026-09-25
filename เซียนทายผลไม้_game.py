f4cc724663683b373bbcb54674026e039c378b9b
404-05-waratchaya <54262@yupparaj.ac.th>
2026-09-25 13:22:34 +0700

    Update เซียนทายผลไม้_game.py

--- เซียนทายผลไม้_game.py
+++ เซียนทายผลไม้_game.py
@@ -40,7 +40,7 @@ def show_result_dialog(ans1, ans2, ans3, ans4, ans5):
     u_ans2 = ans2.strip().lower()
     u_ans3 = ans3.strip().lower()
     u_ans4 = ans4.strip().lower()
-    u_ans5 = ans4.strip().lower()
+    u_ans5 = ans5.strip().lower()
 
 
     # ตรวจข้อ 1
